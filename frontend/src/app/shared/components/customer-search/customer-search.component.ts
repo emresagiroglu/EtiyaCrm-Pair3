1c23451601c2928692599b5bdc00d37d9203fe8a
mustafaceliiikel <69846017+mustafaceliiikel@users.noreply.github.com>
2024-11-06 11:43:43 +0300

    fixed customer search page

--- frontend/src/app/shared/components/customer-search/customer-search.component.ts
+++ frontend/src/app/shared/components/customer-search/customer-search.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { TabsComponent } from '../tabs/tabs.component';
-import { InputComponent } from '../input/input.component';
-import { ButtonComponent } from '../button/button.component';
-import { CustomerTableComponent } from '../customer-table/customer-table.component';
-import { PopupComponent } from '../popup/popup.component';
 import { CommonModule } from '@angular/common';
 import {
   AbstractControl,
@@ -14,12 +9,20 @@ import {
   ValidationErrors,
   ValidatorFn,
 } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { TabsComponent } from '../tabs/tabs.component';
+import { InputComponent } from '../input/input.component';
+import { ButtonComponent } from '../button/button.component';
+import { CustomerTableComponent } from '../customer-table/customer-table.component';
+import { PopupComponent } from '../popup/popup.component';
+
 import { CustomerSearchService } from '../../services/customer-service/customer-search.service';
 import { CustomerSearchResponse } from '../../models/customer/customerSearchResponse';
 import { CustomerSearchRequest } from '../../models/customer/customerSearchRequest';
-import { Router } from '@angular/router';
-import { NgxPaginationModule } from 'ngx-pagination';
 
+import { NgxPaginationModule } from 'ngx-pagination';
+ 
 @Component({
   selector: 'app-customer-search',
   standalone: true,
@@ -39,39 +42,38 @@ import { NgxPaginationModule } from 'ngx-pagination';
 })
 export class CustomerSearchComponent implements OnInit {
   tabs: string[] = ['B2C', 'B2B'];
-
+ 
   showModal: boolean = false;
   showCustomerNotFound: boolean = false;
-
-  searchResulDatas!: CustomerSearchResponse[];
-  paginatedData!: CustomerSearchResponse[];
-
+ 
+  searchResulDatas!: CustomerSearchResponse[]
+ 
   searchForm!: FormGroup;
-
+ 
   activeTab: number = 0;
   totalPages!: number;
-
+ 
   currentPage: number = 1;
   itemsPerPage: number = 5; // Sayfa başına gösterilecek eleman sayısı
-  
-
+ 
+ 
   constructor(
     private formBuilder: FormBuilder,
     public customerSearchService: CustomerSearchService,
     private router: Router
-    
+   
   ) {}
-
+ 
   onPageChange(page: number) {
     this.currentPage = page;
     this.updatePaginatedData();
   }
-
+ 
   ngOnInit(): void {
     this.buildForm();
     this.submitForm();
   }
-
+ 
   buildForm() {
     this.searchForm = this.formBuilder.group({
       nationalityId: ['', [this.numericValidator()]],
@@ -86,7 +88,7 @@ export class CustomerSearchComponent implements OnInit {
       sortOrder: [''],
     });
   }
-
+ 
   setSort(field: string, order: 'asc' | 'desc') {
     this.searchForm.patchValue({
       sortField: field,
@@ -94,82 +96,81 @@ export class CustomerSearchComponent implements OnInit {
     });
     this.submitForm();
   }
-
+ 
   submitForm() {
     this.searchForm.markAllAsTouched();
+   
     if (!this.searchForm.valid) {
       return;
     }
-
+ 
     const customerSearchRequest: CustomerSearchRequest = this.searchForm.value;
-
+ 
     this.customerSearchService.searchCustomer(customerSearchRequest,this.customerSearchService.currentPageService-1).subscribe({
       next: (response: any) => {
         this.searchResulDatas = response.content;
         this.totalPages = response.totalPages;
         this.updatePaginatedData();
 
-        if (response.content.length === 0) {
-          this.showCustomerNotFound= true;
-        }
+        if ((this.searchResulDatas || []).length === 0) {   this.showCustomerNotFound = true; }
+       
       },
     });
   }
   updatePaginatedData() {
     const startIndex = (this.currentPage - 1) * this.itemsPerPage;
     const endIndex = startIndex + this.itemsPerPage;
-        this.paginatedData = this.searchResulDatas.slice(startIndex, endIndex);
       }
-
+ 
   onlyTextValidator(): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
       const isTextOnly = /^[a-zA-Z\s]*$/.test(control.value);
       return isTextOnly ? null : { onlyText: true };
     };
   }
-
+ 
   numericValidator(): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
       const isNumeric = /^[0-9]*$/.test(control.value);
       return isNumeric ? null : { onlyNumeric: true };
     };
   }
-
+ 
   setActiveTab(index: number) {
     this.activeTab = index;
   }
-
+ 
   toggleModal() {
     this.showModal = !this.showModal;
-
+ 
     if (!this.showModal) {
       // Reset the search results when closing the modal
       this.searchResulDatas = [];
     }
   }
-
+ 
   handleButtonClick() {
     this.buildForm()
   }
-
+ 
   goToCustomerCreate() {
     this.router.navigate(['/customer-create']);
   }
-
+ 
   showExitModal() {
     this.showCustomerNotFound = true;
   }
-
+ 
   closeExitModal() {
     this.showCustomerNotFound = false;
   }
-
+ 
   onClickNext() {
     if(this.customerSearchService.currentPageService < this.totalPages){
       this.customerSearchService.currentPageService +=1;
     }
-
-    
+ 
+   
     this.submitForm();
   }
   onClickPrevious(){
@@ -177,6 +178,7 @@ export class CustomerSearchComponent implements OnInit {
       this.customerSearchService.currentPageService -=1
     }
     this.submitForm();
-    
+   
   }
 }
+ 
